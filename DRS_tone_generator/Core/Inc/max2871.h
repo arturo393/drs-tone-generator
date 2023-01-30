bc722bfce25b9627bf1c757915e77518af8852b5
arturo33 <arturo@sigma-telecom.com>
2023-01-30 09:38:24 -0300

    add i2c and m4c64 plugin

---
+++ DRS_tone_generator/Core/Inc/max2871.h
@@ -0,0 +1,152 @@
+/*
+ * max2871.h
+ *
+ *  Created on: Jan 25, 2023
+ *      Author: artur
+ */
+
+#ifndef INC_MAX2871_H_
+#define INC_MAX2871_H_
+
+#include "main.h"
+
+#define LE_LOW()  CLEAR_BIT(GPIOA->ODR, GPIO_ODR_OD1)
+#define LE_HIGH() SET_BIT(GPIOA->ODR, GPIO_ODR_OD1)
+
+//composition of MAX2871 Registers
+
+//Register 1
+#define CPL  0x3UL //Charge pump liniarity 30%
+#define CPT  0x00UL //Charge pump test mode  normal mode
+#define PHASE  0x1UL //Phase Value (recomened)
+#define MODULUS  0xFA0UL //4000 for max resolution
+#define ADDR1  0x1UL
+
+//Register 2
+#define LDS  0x1UL
+// 1 if fPFD > 32 MHz
+#define SDN  0x0UL
+//noise mode  Low-noise mode
+#define MUX  0x6UL
+//MUX pin configuration  Digital lock detect
+#define DBR  0x0UL
+//reference doubler is disabled
+#define RDIV2  0x0UL
+//reference divide-by-2 is disabled
+#define RCNT  0x0UL
+// reference divide Value is unused
+#define REG4DB  0x0UL
+//double buffer mode disabled
+#define CP  0x00UL
+//charge pump current  0.32 mA (1.36/RSET * (1 + CP[3:0]) RSET  5k1)
+#define LDF  0x0UL
+// lock dtect function  Frac-N lock detect
+#define LDP  0x0UL
+// lock detect precision  10ns
+#define PDP  0x1UL
+//phase detector polarity set poitive
+#define SHDN  0x0UL
+//power down mode  normal mode
+#define TRI  0x0UL
+//charge pump output high-impedance mode disabled
+#define RST  0x0UL
+// counter reset mode  normal operation
+#define ADDR2  0x2UL
+
+//Register 3
+#define VCO_MS  0x0UL
+// VCO maual selction: unused
+#define VAS_SHDN  0x0UL
+//VAS enabled
+#define VAS_TEMP  0x1UL
+//VAS temperature compensation enabled
+#define CSM  0x0UL
+//Cycle slip mode disabled
+#define MUTEDEL  0x0UL
+//mute delay mode disabled
+#define CDM  0x1UL
+// Fast-lock mode enabled
+#define CDIV  0x0UL
+// clock divider value unused
+#define ADDR3  0x3UL
+
+//Register 4
+#define RES  0x3UL
+//Reserved
+#define SDLDO  0x0UL
+//LDO endabled
+#define SDDIV  0x0UL
+//VCO Divider enabled
+#define SDREF  0x0UL
+//Reference input enabled
+#define FB  0x1UL
+//VCO to N counter mode is NOT divided
+
+#define BS  0x30FFUL
+//shoud be choosen so that fPFD/BS  50kH or less
+#define SDVCO  0x0UL
+//VCO enabled
+#define MTLD  0x0UL
+//RFOUT Mute until Lock detet mode disabled
+#define BDIV  0x0UL
+//RFOUTB is divided (so it's the same as RFOUTA)
+#define RFB_EN  0x1UL
+//RFOUTB enabled
+#define BPWR  0x3UL
+//RFOUTB  5 dBm
+#define RFA_EN  0x1UL
+//RFOUTA enabled
+#define APWR  0x3UL
+//RFOUTA  5dBm
+#define ADDR4  0x4UL
+
+//Register 5
+#define VAS_DLY  0x3UL
+//0x0 if VAS_TEMP  0, 0x3 if VAS_TEMP  1
+#define SDPLL  0x0UL
+// PLL enabled
+#define F01  0x1UL
+// if F  0 then int
+#define LD  0x3UL
+//Lock-Detect pin function  HIGH
+#define MUX_MSB  0x0UL
+//MSB of MUX
+#define ADCS  0x0UL
+//ADC normal operation (ADC isn't used)
+#define ADCM  0x0UL
+//ADC disabled
+#define ADDR5  0x5UL
+
+#define FREQ_OUT_SIZE 8
+#define LACT_SIZE 2
+
+typedef struct MAX2871{
+	unsigned long long FreqOUT;
+	unsigned long long FreqOUTold;
+	unsigned long long FMIN;
+	unsigned long long FMAX;
+	float LMAX;
+	float LMIN;
+	float LACT;
+	float ATT;
+	float CAL;
+	unsigned long DIVA;
+	REGISTER0_t *register0;
+}MAX2871_t;
+
+
+typedef struct REGISTER0{
+	//Register 0
+	unsigned long INT; //Enables fractional-N mode
+	unsigned long NDIV;
+	unsigned long FRAC;
+	unsigned long ADDR0;
+}REGISTER0_t ;
+
+
+void max2871Init();
+void max2871Write(unsigned long data);
+void shiftOut(uint32_t DATAPIN, uint32_t CLOCKPIN, uint32_t MSBFIRST,
+		uint32_t data);
+
+#endif /* INC_MAX2871_H_ */
---
+++ DRS_tone_generator/Core/Src/main.c
@@ -0,0 +1,495 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "i2c1_master.h"
+#include "max2871.h"
+#include "m24c64.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+CRC_HandleTypeDef hcrc;
+
+I2C_HandleTypeDef hi2c1;
+
+SPI_HandleTypeDef hspi2;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_CRC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+// PINS MAX2871
+/*
+ #define CLOCKPIN A0
+ #define DATAPIN A1
+ #define LE A2
+ #define CE A3
+ */
+
+float AC[56];
+float EEPROM_float;
+
+// OLED 128x64 with SH1106 Controller
+// E.G. DM-OLED13-625
+/*
+ #define OLED_MOSI  29
+ #define OLED_CLK   27
+ #define OLED_DC    35
+ #define OLED_CS    37
+ #define OLED_RESET 31
+ Adafruit_SH1106 display(OLED_MOSI,OLED_CLK,OLED_DC,OLED_RESET,OLED_CS);
+ */
+/*
+ #if (SH1106_LCDHEIGHT != 64)
+ #error("Height incorrect, please fix Adafruit_SH1106.h!");
+ #endif
+
+ unsigned int ActiveDisplay = 1;  // AMPLITUDE
+ boolean ShowCursor = true;
+ unsigned int CursorPosition = 2;
+
+ // Menue Intem to be displayed
+ unsigned int MenueItem = 0;
+
+ // STATE OF THE ROTARY ENCODER
+ const int RE2 = 49;  // PRESSED
+ const int RE1 = 47;
+ const int RE0 = 45;
+ unsigned long StartMilli;
+ unsigned long DurationMilli;
+ boolean PressedLong = false;
+ */
+// "INTERRUPT" VARIABLES
+unsigned int RE_now = 0;
+unsigned int RE_old = 0;
+unsigned int RE_xor = 0;
+unsigned int RE_one = 0;
+unsigned int RE_two = 0;
+unsigned int RE_eval = 0x0;  // 0 = LEFT(-), 1=RIGHT(+), 2=PRESSED
+unsigned long RE_time0 = 0;
+unsigned long RE_time1 = 0;
+unsigned long RE_time2 = 0;
+
+const int MCPADR = 0x27;
+const int ATTREGADR = 0x12;    // ATTENUATOR, MCP REG A
+const int CALREGADR = 0x13;    // CALIBRATION, MCP REG B
+unsigned int CALBYTE;
+unsigned int ATTBYTE;
+
+const int EEPROM_ADR = 0x50;
+uint8_t addrList[5] = { 0 };
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
+	MAX2871_t ppl;
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+//	MX_I2C1_Init();
+	MX_SPI2_Init();
+	MX_USART1_UART_Init();
+	MX_CRC_Init();
+	/* USER CODE BEGIN 2 */
+	i2c1MasterInit();
+	max2871Init(&ppl);
+	uint8_t addrList[5] = { 0 };
+
+	uint8_t freq_init[FREQ_OUT_SIZE] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66,
+			0x77, 0x88 };
+
+	m24c64WriteNBytes(BASE_ADDR, freq_init, FREQ_OUT_ADDR, FREQ_OUT_SIZE);
+	uint8_t buffer[10] = { 0 };
+
+	m24c64ReadNBytes(BASE_ADDR, buffer, FREQ_OUT_ADDR, FREQ_OUT_SIZE);
+
+	for (int i = FREQ_OUT_SIZE - 1; i >= 0; i--)
+		ppl.FreqOUT |= (buffer << (i * 8));
+
+	m24c64ReadNBytes(BASE_ADDR, buffer, FREQ_OUT_ADDR + FREQ_OUT_SIZE,
+	LACT_SIZE);
+	ppl.LACT = buffer[0] + buffer[1] / 2.0;
+
+	if (ppl.FreqOUT == -1) {
+		ppl.FreqOUT = 4000000000;
+		ppl.LACT = 12;
+	}
+
+	HAL_Delay(200);
+	CalculateRegisterValues();
+
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
+		test[0] = test[0] + 1;
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/** Configure the main internal regulator output voltage
+	 */
+	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief CRC Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_CRC_Init(void) {
+
+	/* USER CODE BEGIN CRC_Init 0 */
+
+	/* USER CODE END CRC_Init 0 */
+
+	/* USER CODE BEGIN CRC_Init 1 */
+
+	/* USER CODE END CRC_Init 1 */
+	hcrc.Instance = CRC;
+	hcrc.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_ENABLE;
+	hcrc.Init.DefaultInitValueUse = DEFAULT_INIT_VALUE_ENABLE;
+	hcrc.Init.InputDataInversionMode = CRC_INPUTDATA_INVERSION_NONE;
+	hcrc.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
+	hcrc.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
+	if (HAL_CRC_Init(&hcrc) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN CRC_Init 2 */
+
+	/* USER CODE END CRC_Init 2 */
+
+}
+
+/**
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init(void) {
+
+	/* USER CODE BEGIN I2C1_Init 0 */
+
+	/* USER CODE END I2C1_Init 0 */
+
+	/* USER CODE BEGIN I2C1_Init 1 */
+
+	/* USER CODE END I2C1_Init 1 */
+	hi2c1.Instance = I2C1;
+	hi2c1.Init.Timing = 0x00303D5B;
+	hi2c1.Init.OwnAddress1 = 0;
+	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	hi2c1.Init.OwnAddress2 = 0;
+	hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Configure Analogue filter
+	 */
+	if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+
+	/** Configure Digital filter
+	 */
+	if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN I2C1_Init 2 */
+
+	/* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI2_Init(void) {
+
+	/* USER CODE BEGIN SPI2_Init 0 */
+
+	/* USER CODE END SPI2_Init 0 */
+
+	/* USER CODE BEGIN SPI2_Init 1 */
+
+	/* USER CODE END SPI2_Init 1 */
+	/* SPI2 parameter configuration*/
+	hspi2.Instance = SPI2;
+	hspi2.Init.Mode = SPI_MODE_MASTER;
+	hspi2.Init.Direction = SPI_DIRECTION_1LINE;
+	hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+	hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+	hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi2.Init.NSS = SPI_NSS_SOFT;
+	hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+	hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	hspi2.Init.CRCPolynomial = 7;
+	hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+	hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+	if (HAL_SPI_Init(&hspi2) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN SPI2_Init 2 */
+
+	/* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
+
+	/* USER CODE BEGIN USART1_Init 0 */
+
+	/* USER CODE END USART1_Init 0 */
+
+	/* USER CODE BEGIN USART1_Init 1 */
+
+	/* USER CODE END USART1_Init 1 */
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 115200;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART1_Init 2 */
+
+	/* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOB, RS485_CTRL_Pin | LED_2_Pin | LED_3_Pin,
+			GPIO_PIN_RESET);
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOA,
+	MAX_LE_Pin | MAX_CE_Pin | MAX_MUX_Pin | MAX_RF_ENABLE_Pin | LED_1_Pin,
+			GPIO_PIN_RESET);
+
+	/*Configure GPIO pins : RS485_CTRL_Pin LED_2_Pin LED_3_Pin */
+	GPIO_InitStruct.Pin = RS485_CTRL_Pin | LED_2_Pin | LED_3_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : TEST_INPUT_1_Pin TEST_INPUT_2_Pin SW_2_Pin */
+	GPIO_InitStruct.Pin = TEST_INPUT_1_Pin | TEST_INPUT_2_Pin | SW_2_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : MAX_LE_Pin MAX_CE_Pin MAX_MUX_Pin MAX_RF_ENABLE_Pin
+	 LED_1_Pin */
+	GPIO_InitStruct.Pin = MAX_LE_Pin | MAX_CE_Pin | MAX_MUX_Pin
+			| MAX_RF_ENABLE_Pin | LED_1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : MAX_LOCK_DETECTOR_Pin SW_1_Pin SW_3_Pin SW_4_Pin
+	 SW_5_Pin */
+	GPIO_InitStruct.Pin = MAX_LOCK_DETECTOR_Pin | SW_1_Pin | SW_3_Pin | SW_4_Pin
+			| SW_5_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : SW_0_Pin SW_6_Pin SW_8_Pin SW_9_Pin */
+	GPIO_InitStruct.Pin = SW_0_Pin | SW_6_Pin | SW_8_Pin | SW_9_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/*Configure GPIO pin : SW_7_Pin */
+	GPIO_InitStruct.Pin = SW_7_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(SW_7_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
